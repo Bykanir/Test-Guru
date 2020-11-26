0786668d13846c1421da40382e5abed33e5ab581
Bykanir <bykanir@gmail.com>
2020-11-26 16:16:08 +0300

    fix method model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
-  def tests_by_level(level)
+  def passed_tests_by_level(level)
     Test.joins('JOIN user_tests ON user_tests.test_id = tests.id')
-      .where(tests: { level: level})
+      .where(user_tests: { user_id: self.id }, tests: { level: level})
   end
 end
