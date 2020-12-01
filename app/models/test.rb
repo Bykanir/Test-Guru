bee1bcc91f63bf9d93c45e81fac7e3a144ef0cb0
Bykanir <bykanir@gmail.com>
2020-12-01 22:52:50 +0300

    fix associations model test, user and passed_tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,10 +1,9 @@
 class Test < ApplicationRecord
   belongs_to :category
-  belongs_to :user
+  belongs_to :user, class_name: 'Author'
 
   has_many :questions, dependent: :destroy
-
-  has_and_belongs_to_many :passed_tests
+  has_many :users, through: :passed_tests
 
   def self.tests_by_category(category)
     joins('JOIN categories ON tests.category_id = categories.id')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_and_belongs_to_many :passed_tests
-  has_and_belongs_to_many :tests
+  has_many :tests, through: :passed_tests
+  has_many :tests, dependent: :destroy
 
   def passed_tests_by_level(level)
     Test.joins('JOIN user_tests ON user_tests.test_id = tests.id')
