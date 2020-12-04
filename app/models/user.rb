28b2bd9fae45b26682dea933954defaba09e760e
Bykanir <bykanir@gmail.com>
2020-12-04 22:12:23 +0300

    fix association model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :tests, through: :passed_tests
-  has_many :tests, dependent: :destroy
+  has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
 
   def passed_tests_by_level(level)
     Test.joins('JOIN user_tests ON user_tests.test_id = tests.id')
