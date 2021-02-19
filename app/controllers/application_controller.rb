835d9c2202c71c8e4d26a6bf31ee5394d3b17763
Bykanir <bykanir@gmail.com>
2021-02-19 21:42:07 +0300

    add localization in project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@
 
 class ApplicationController < ActionController::Base
 
+  before_action :set_locale
+
   def after_sign_in_path_for(resource)
    stored_location_for(:admin) ||
       if resource.is_a?(Admin)
@@ -9,6 +11,16 @@ class ApplicationController < ActionController::Base
       else
         super
       end
+  end
+  
+  def default_url_options
+    I18n.locale == I18n.default_locale ? {} : { lang: I18n.locale }
+  end
+  
+  private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
   
 end
