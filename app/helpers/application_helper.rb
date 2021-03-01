7b208050cccd37fabb7182cc1392aa84ced140fe
Bykanir <bykanir@gmail.com>
2021-03-01 20:44:15 +0300

    add bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,21 @@
 
 module ApplicationHelper
 
+  ALERTS = { 'notice' => 'alert-primary', 
+              'alert' => 'alert-warning', 
+              'succes' => 'alert-success', 
+              'error' => 'alert-danger' }
+
   def current_year
     Time.current.year
   end
 
   def github_url(author, repo)
     "https://github.com/#{author}/#{repo}"    
+  end
+
+  def flash_message(key, message)
+    content_tag :div, message, class: "alert #{ALERTS[key]}", role: "alert"
   end
 
 end
