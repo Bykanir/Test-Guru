892e47635e632ef50d995e78bfd85f532317d6be
bykanir <bykanir@gmail.com>
2021-07-02 14:54:47 +0300

    add migration for index tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_112812) do
+ActiveRecord::Schema.define(version: 2021_07_02_114331) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
     t.string "title", null: false
@@ -67,6 +70,7 @@ ActiveRecord::Schema.define(version: 2021_05_18_112812) do
     t.integer "author_id"
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
   create_table "users", force: :cascade do |t|
