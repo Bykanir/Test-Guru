fce725758c4f714cf9600285c0d14af6cd4ce3a9
bykanir <bykanir@gmail.com>
2021-07-02 14:07:44 +0300

    correction of input parametres registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 class ApplicationController < ActionController::Base
 
   before_action :set_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
    stored_location_for(:admin) ||
@@ -21,6 +22,10 @@ class ApplicationController < ActionController::Base
 
   def set_locale
     I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
   
 end
